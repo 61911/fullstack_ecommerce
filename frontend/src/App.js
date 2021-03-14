210601282f383071097e450795f83257a700843d
parth <psp.11.jb@gmail.com>
2021-03-15 00:21:06 +0530

    Initial Stage[added frontend-{reactJS}]

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <h1>Welcome!!</h1>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
